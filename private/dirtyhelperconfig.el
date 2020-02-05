9d81e762fee6c9e2227e43fafd4a30ec8729336f
frido <frido@frido>
2020-02-05 07:52:44 +0100

    sync 1580885564

--- private/dirtyhelperconfig.el
+++ private/dirtyhelperconfig.el
@@ -16,11 +16,12 @@
 ;; Meine dirty-little-helpers Hydra
 (defhydra fm/hydra-helpers (:color blue :hint nil)
   "
- _f_: fill-mode  _w_: wspace-cleanup _m_: megamailfun  _q_: quit
+ _f_: fill-mode  _w_: wspace-cleanup _m_: mail-mode _M_: megamailfun  _q_: quit
   "
   ("f" auto-fill-mode)
   ("w" whitespace-cleanup)
-  ("m" mymegamailfun)
+  ("m" mail-mode)
+  ("M" mymegamailfun)
   ("q" nil :color blue))
 
 (bind-key (kbd "<f12>") 'fm/hydra-helpers/body)
